ec1d4b59cb2b471408ab25492c6fa5975bf3bd08
Tariq <tariq86hamnid@gmail.com>
2022-06-17 07:33:13 -0700

    changed header file name

---
+++ monty.h
@@ -0,0 +1,50 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* utils */
+void get_opcode_funcs(stack_t **stack, unsigned int line_num, char *token);
+void free_stack(stack_t *stack);
+
+/* opcode functions */
+void _push(stack_t **stack, unsigned int line_num);
+void _pall(stack_t **stack, unsigned int line_num);
+
+#endif
